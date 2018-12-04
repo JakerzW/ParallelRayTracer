57e3577490ddae081b75c35a6e595dbc2d36da4d
Jake Walder <jakerzw@hotmail.co.uk>
2018-12-04 14:14:26 +0000

    CMake working

--- src/prt/main.cpp
+++ src/prt/main.cpp
@@ -1,10 +1,45 @@
+#include <SDL2/SDL.h>
+
 #include <iostream>
 
 int main()
 {
 	std::cout << "Doing nothing..." << std::endl;
 
-	system("PAUSE");
+	SDL_Init(SDL_INIT_VIDEO);
+	
+	bool running = true;
+
+	while (running)
+	{
+		SDL_Event event = { 0 };
+		
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+				case SDL_QUIT:
+				{
+					running = false;
+					break;
+				}
+				case SDL_KEYDOWN:
+				{
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_ESCAPE:
+						{
+							running = false;
+							break;
+						}
+					}
+				}
+			}
+			break;
+		}
+	}
+
+	//system("PAUSE");
 
 	return 0;
 }
