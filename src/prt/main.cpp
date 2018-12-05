0c1283ef3889b4e40f720060964ea3bd77e79f6a
Jake Walder <jakerzw@hotmail.co.uk>
2018-12-05 17:27:03 +0000

    SDL setup with fps counter working

--- src/prt/main.cpp
+++ src/prt/main.cpp
@@ -1,4 +1,6 @@
 #include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <ext.hpp>
 
 #include <iostream>
 
@@ -6,13 +8,43 @@ int main()
 {
 	std::cout << "Doing nothing..." << std::endl;
 
-	SDL_Init(SDL_INIT_VIDEO);
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "Something went wrong, cannot initialise SDL." << std::endl;
+		return -1;
+	}
+	
+	int winPosX = 100;
+	int winPosY = 100;
+	int winWidth = 1280;
+	int winHeight = 720;
+
+	SDL_Window* window = SDL_CreateWindow("My Window", winPosX, winPosY, winWidth, winHeight,
+										  SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Something went wrong, cannot initialise glew." << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
 	
 	bool running = true;
 
+	unsigned int lastTime = SDL_GetTicks();
+	unsigned int currentTime;
+	float deltaTime;
+	int fps = 60;
+	float idealFps = (float)(1.0f / fps);
+
 	while (running)
 	{
-		SDL_Event event = { 0 };
+		SDL_Event event;
 		
 		while (SDL_PollEvent(&event))
 		{
@@ -36,6 +68,23 @@ int main()
 				}
 			}
 			break;
+		}
+
+		currentTime = SDL_GetTicks();
+		deltaTime = (float)(currentTime - lastTime) / 1000.0f;
+		lastTime = currentTime;
+
+		//draw
+
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+
+		if (idealFps > deltaTime)
+		{
+			//std::cout << "Delaying..." << std::endl;
+			unsigned int delay = (idealFps - deltaTime) * 1000.0f;
+			SDL_Delay(delay);
 		}
 	}
 
